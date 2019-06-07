e36ed6f752c6ef17943e8adfc1b03e9ddd85430c
Luis Camacho <luis.camacho@itcparts.com>
2019-06-07 12:37:10 +0100

    set site to maintenance

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,14 +5,14 @@ import Image from "../components/image"
 
 const IndexPage = () => (
   <Layout>
-    <div style={{padding:'1em'}}>
-      <h1 style={{display: 'inline', width: `100%`}} id="header">#Somos Natureza</h1>
-      <div style={{right: `0px`, position: 'fixed', margin: '-2.5em 1em'}}>
+    <div style={{padding:'1em', display:'flex'}}>
+      <h1 style={{display: 'inline', width: `100%`, margin: `28vh 0`}} id="header">#SomosNatureza</h1>
+      {/* <div style={{right: `0px`, position: 'fixed', margin: '-2.5em 1em'}}>
         <h1 style={{display: 'inline'}} id="header">Blog</h1>
         <h1 style={{display: 'inline'}} id="header">Eventos</h1>
-      </div>
+      </div> */}
     </div>
-    <Image />
+    {/* <Image /> */}
     
   </Layout>
 )
